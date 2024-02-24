b8184e6321ea0aa4ac8397a00ab7a918bfd48087
JAVAHTML1 <83888041+JAVAHTML1@users.noreply.github.com>
2024-02-23 20:47:39 -0500

    Recode to use base "bcrypt" library. IN PROGRESS

--- auth.py
+++ auth.py
@@ -1,23 +1,25 @@
-from passlib.hash import bcrypt
+import bcrypt
 
 class auth:
-    def __init__(self, user, passwd, setup):
-        self.hasher = bcrypt.using()
-        self.user = user
-        self.passwd = passwd
-        self.setup = setup
+    def __init__(self, user, passwd):
+        self.user = user.encode("utf-8")
+        self.passwd = passwd.encode("utf-8")
         self.correct = False
     def setupd(self):
-        userw = self.hasher.hash(self.user)
-        passwdw = self.hasher.hash(self.passwd)
+        userw = bcrypt.hashpw(self.user, bcrypt.gensalt())
+        passwdw = bcrypt.hashpw(self.passwd, bcrypt.gensalt())
         with open("auth.txt", "a") as authfile:
             authfile.write(f"{userw},{passwdw}\n")
     def login(self):
         with open("auth.txt", "r") as authfile:
             line = authfile.readline()
             userf, passwdf = line.split(",")
-        if self.hasher.verify(self.user, userf) and self.hasher.verify(self.passwd, passwdf):
+
+#            usef = int(userf)
+#            passwdf = int(passwdf)
+        if bcrypt.checkpw(self.passwd, passwdf) and bcrypt.checkpw(self.user, userf):
             self.correct = True
-            return self.correct
-auther = auth("User", "Password", False)
+
+auther = auth("User", "Password")
+#auther.setupd()
 print(auther.login())
